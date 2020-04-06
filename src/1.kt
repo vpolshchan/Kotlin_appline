3b1c3681bb83d2fe11ca7267a564546cc8d9e0de
vpolshchan <vpolshchan@gmail.com>
2020-04-06 19:07:17 +0300

    Задание 1: Переменные

---
+++ src/1.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    var any: Any? = null
+    any = 7
+    val clon = any/2.5
+    println(clon) //Float - хранит число с плавающей точкой от -3.4*1038 до 3.4*1038 и занимает 4 байта
+}
