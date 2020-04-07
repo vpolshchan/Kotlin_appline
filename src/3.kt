690e781fc34ae490edb737e00592c33be5d1d498
vpolshchan <vpolshchan@gmail.com>
2020-04-07 16:38:29 +0300

    Задание 3: Преобразование

---
+++ src/3.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    //Задание 3
+    val five: Char = '5'
+    println(five.toString())
+    println(five.toByte())
+    println(five.toShort())
+    println(five.toInt())
+    println(five.toDouble())
+    println(five.toFloat())
+    println(five.toLong())
+}
