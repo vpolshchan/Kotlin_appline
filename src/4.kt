dc19251fea3edc5b39cee35682b626b3a3ab87ad
vpolshchan <vpolshchan@gmail.com>
2020-04-07 16:48:53 +0300

    Задание 4: Приветствие пользователя

---
+++ src/4.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    //Задание 4
+    println("Введи свое имя и нажми Enter:")
+    var userNameIn: String? = readLine()
+    var textOut: String = userNameIn.toString()
+    print("Привет, $textOut!")
+}
