08691e2d8738d6111adc37098f67498eed827014
vpolshchan <vpolshchan@gmail.com>
2020-05-05 12:16:15 +0300

    Задание 22: Авиакомпания 6. Вывод информации

--- src/17.kt
+++ src/17.kt
@@ -1,18 +1,15 @@
 fun main(args: Array<String>) {
-    //Задание 17+18+19+20+21
-    //var Aircraft: Aircraft = Aircraft(1,14140,10100)
+    //Задание 17+18+19+20+21+22
     var Boing: Boeing747 = Boeing747(2,14815,242470,581)
-    //println(Aircraft.info())
     println(Boing.info())
-    println("вместимость пассажиров в Boeing747 ${Boing.pascapacity}")
 }
 abstract class Aircraft(_number: Int, _range: Int, _capacity: Int){
-    var number: Int = _number
-    var range: Int = _range
-    var capacity: Int = _capacity
-    val fuel: Int
+    protected var number: Int = _number
+    protected var range: Int = _range
+    protected var capacity: Int = _capacity
+    protected val fuel: Int
         get() = (capacity/range)*100
-    fun info(): String{
+    open fun info(): String{
         var i: String = """
             номер воздушного судна $number,
             максимальная дальность полета $range,
@@ -26,6 +23,14 @@ interface Passenger {
     var pascapacity: Int
 }
 class Boeing747(_number: Int, _range: Int, _capacity: Int, override var pascapacity: Int): Aircraft(_number, _range, _capacity), Passenger{
-
-
+ override fun info(): String {
+     var i: String = """
+            номер воздушного судна $number,
+            максимальная дальность полета $range,
+            вместимость бака $capacity,
+            расход топлива на 100 км = $fuel
+            вместимость пассажиров $pascapacity
+        """.trimIndent()
+     return i
+ }
 }
